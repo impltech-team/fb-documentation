71e0e8b81ba792d44360bb572663d073378826d7
AlexLisovenko-limeup <alex.l@limeup.io>
2025-04-08 10:38:15 +0300

    adding controllers to trigger data prefetching

--- src/main/java/io/limeup/flexbets/sport/api/StatsController.java
+++ src/main/java/io/limeup/flexbets/sport/api/StatsController.java
@@ -2,10 +2,7 @@ package io.limeup.flexbets.sport.api;
 
 import io.limeup.flexbets.sport.dto.StatsBatchRequestDTO;
 import io.limeup.flexbets.sport.dto.StatsResponseDTO;
-import io.limeup.flexbets.sport.service.AreaService;
-import io.limeup.flexbets.sport.service.SportService;
 import io.limeup.flexbets.sport.service.StatsService;
-import io.limeup.flexbets.sport.service.VenueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,25 +16,12 @@ import java.util.List;
 @RequestMapping("/v1/stats")
 @RequiredArgsConstructor
 public class StatsController {
+
     private final StatsService statsService;
-    private final AreaService areaService;
-    private final SportService sportService;
-    private final VenueService venueService;
 
     @PostMapping("/batch")
     public ResponseEntity<List<StatsResponseDTO>> listBatchStats(@RequestBody StatsBatchRequestDTO request) {
         return ResponseEntity.ok(statsService.listBatchStats(request));
     }
 
-    @PostMapping("/test")
-    public void test() {
-        statsService.fetchStatData(1);
-    }
-
-    @PostMapping("/test2")
-    public void test2() {
-        areaService.fetchAreaData();
-        venueService.fetchVenueData();
-        sportService.fetchSportData();
-    }
 }
