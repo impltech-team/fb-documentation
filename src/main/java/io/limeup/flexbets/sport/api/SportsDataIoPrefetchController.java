925cf30d6354461a0ab94b2cf9d7dcab55d936ce
Taras Kukhar <taras.kukhar@meliorgames.com>
2025-06-26 11:44:53 +0300

    make changes

--- src/main/java/io/limeup/flexbets/sport/api/SportsDataIoPrefetchController.java
+++ src/main/java/io/limeup/flexbets/sport/api/SportsDataIoPrefetchController.java
@@ -26,42 +26,42 @@ public class SportsDataIoPrefetchController {
     public ResponseEntity<PrefetchResponse> prefetchPlayers() {
         taskExecutor.execute(importService::importPlayers);
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("importPlayers", Instant.now()));
+                .body(new PrefetchResponse("io/players", Instant.now()));
     }
 
     @PostMapping("/teams")
     public ResponseEntity<PrefetchResponse> prefetchTeams() {
         taskExecutor.execute(importService::importTeams);
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("teams", Instant.now()));
+                .body(new PrefetchResponse("io/teams", Instant.now()));
     }
 
     @PostMapping("/scores")
     public ResponseEntity<PrefetchResponse> prefetchScores() {
         taskExecutor.execute(() -> importService.importScores(LocalDate.now()));
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("scores", Instant.now()));
+                .body(new PrefetchResponse("io/scores", Instant.now()));
     }
 
     @PostMapping("/player-stats")
     public ResponseEntity<PrefetchResponse> prefetchPlayerStats() {
         taskExecutor.execute(importService::importPlayersStats);
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("player-stats", Instant.now()));
+                .body(new PrefetchResponse("io/player-stats", Instant.now()));
     }
 
     @PostMapping("/player-game-stats")
     public ResponseEntity<PrefetchResponse> prefetchPlayerGameStats() {
         taskExecutor.execute(importService::importPlayerGameStats);
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("player-game-stats", Instant.now()));
+                .body(new PrefetchResponse("io/player-game-stats", Instant.now()));
     }
 
     @PostMapping("/bet-markets")
     public ResponseEntity<PrefetchResponse> prefetchBetMarkets() {
         taskExecutor.execute(() -> importService.importBetMarkets(LocalDate.now()));
         return ResponseEntity.accepted()
-                .body(new PrefetchResponse("bet-markets", Instant.now()));
+                .body(new PrefetchResponse("io/bet-markets", Instant.now()));
     }
 
 
